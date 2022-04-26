0c32eeac5d625f1e20ed9ababcab6f5eab29f152
FerdyRY <ferdysr1140@gmail.com>
2022-04-26 10:00:33 +0800

    asuauausua

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+    token: "OTYwNTEwMzg3MDk3MzEzMzEw.YkrfAQ.TYwRs_Ohd3wYY4ivMU-ljRnSEls",
+    prefix: process.env.PREFIX
+}
+// I have made all of this .env !
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by R J | https://discord.gg/8fYUFxMtAq
+ * @INFO
+ * Work for Milanio Development | https://discord.gg/8fYUFxMtAq
+ * @INFO
+ * Please Mention Us Milanio Development, When Using This Code!
+ * @INFO
+ *********************************************************/
